5daccecb61f2c43fe9c3e1b4b19758380e34008a
sergey <sergey_sivakov_84@mail.ru>
2022-10-13 20:39:37 +0300

    +

--- client.py
+++ client.py
@@ -3,8 +3,9 @@ import logging
 fp = urllib.request.urlopen('http://127.0.0.1:5000/todo/weather/now')
 encodedContent = fp.read()
 decodedContent = encodedContent.decode("utf8")
-
-
 print(decodedContent)
-logging.basicConfig(filename='test.log', level=logging.INFO)
-logging.info('decodedContent')
+def test():
+    logging.basicConfig(filename='test.log', level=logging.INFO, format = "%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s")
+    a = logging.getLogger(__name__)
+    logging.info(decodedContent)
+test()
